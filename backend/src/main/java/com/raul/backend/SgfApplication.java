a67a40b3598d19aa2bc1871135be6854c8a7af30
Raul-guii <raulawp460@gmail.com>
2026-06-06 14:54:14 -0300

    fix timezone

--- backend/src/main/java/com/raul/backend/SgfApplication.java
+++ backend/src/main/java/com/raul/backend/SgfApplication.java
@@ -1,12 +1,20 @@
 package com.raul.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class SgfApplication {
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SgfApplication.class, args);
